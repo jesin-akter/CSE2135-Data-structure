ffd4ebf664772c458ed930c7cae89ce6fe062eb5
Jesin <107608184+jesin-akter@users.noreply.github.com>
2022-08-04 19:37:56 +0600

    Add files via upload

---
+++ Lab 2_array implementation & deletion/Lab-2.cpp
@@ -0,0 +1,40 @@
+
+
+#include<stdio.h>
+main()
+{
+    int arr[100];
+    int i,j,n;
+    
+    printf("Enter the size of the Array: ");
+    scanf("%d",&n);
+    for(int i = 0; i<=n-1; i++){
+        printf("Enter index %d: ", i);
+    scanf("%d",&arr[i]);
+    }
+    printf("\n\nTotal data in the Array is: \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    printf("\n");
+  
+    printf("\nEnter the position you want to delete: ");
+    scanf("%d",&j);
+    while(j<n)
+    {
+        arr[j-1]=arr[j];
+        j++;
+    }
+    n-=1;
+    printf("\n\nAfter deletion the exist data in the list is: \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    printf("\n\n");
+    return 0;
+}
+
+
+    
